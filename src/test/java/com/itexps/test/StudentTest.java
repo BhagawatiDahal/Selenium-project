ca7d4971ae6d66f2d091e34f487cc7f4eb2d6ac5
BhagawatiDahal <brave.bani@gmail.com>
2021-08-03 21:46:05 -0500

    first commit

---
+++ src/test/java/com/itexps/test/StudentTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.test;
+
+import com.itexps.vo.PartTimeStudent;
+import com.itexps.vo.Student;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author brave
+ */
+public class StudentTest {
+    
+    Student stu;  // declare
+    
+    
+    
+    public StudentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+       stu = new PartTimeStudent(1550.0f,true,1, 'A',"Tom"); 
+        
+        
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+    public void testStudent() {
+        
+      assertEquals("Tom",stu.getFirst());
+        
+  assertTrue(((PartTimeStudent) stu).isWorking());
+    }
+}
